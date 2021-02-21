0819ad7a44c2988848ee03479fcb19f4d0307468
Sami Jouppila <sami.jouppila@gmail.com>
2021-02-21 17:28:43 +0200

    Login service GET /api/auth/login

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const authRouter = express.Router();
+const axios = require('axios');
+const login_url = process.env.LOGIN_SERVICE_URL;
 
 const {
     issueNewJwtToken,
     authenticateHttpBasic
 } = require('../controllers/authController');
 
-authRouter.get('/login', authenticateHttpBasic, issueNewJwtToken)
-
-/*authRouter.get('/login', function(req, res) {
+authRouter.get('/login', async function(req, res) {
     const axiosConfig = {
         headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
+            "Content-Type": "application/json"
         },
     };
-
-    const response = await axios.get()
-})*/
+    if (req.headers['authorization']) {
+        axiosConfig.headers.Authorization = req.headers.authorization;
+    }
+    try {
+        const response = await axios.get(encodeURI(login_url + '/api/auth/login'), axiosConfig);
+        res.status(response.status).send(response.data);
+    } catch (error) {
+        res.status(error.response.status).send(error.response.data);
+    }
+})
 
 module.exports = authRouter;
